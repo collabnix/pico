e0a91a0e319a182d64d290dc0d17a41fbb5a5ce9
Avinash Bendigeri <abendigeri@gmail.com>
2019-07-20 19:13:54 +0530

    Update consumer.py
    Fixed with the bugs and added the ids

--- deployment/raspbi/consumer.py
+++ deployment/raspbi/consumer.py
@@ -9,7 +9,7 @@ brokers = ["35.189.130.4:9092"]
 consumer = KafkaConsumer(
     topic, 
     bootstrap_servers=brokers,
-    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 
 # Set the consumer in a Flask App
@@ -21,6 +21,7 @@ def index():
 
 @app.route('/camera_1', methods=['GET'])
 def camera_1():
+    id=5
     """
     This is the heart of our video display. Notice we set the mimetype to 
     multipart/x-mixed-replace. This tells Flask to replace any old images with 
@@ -32,6 +33,7 @@ def camera_1():
 
 @app.route('/camera_2', methods=['GET'])
 def camera_2():
+    id=6
     """
     This is the heart of our video display. Notice we set the mimetype to 
     multipart/x-mixed-replace. This tells Flask to replace any old images with 
@@ -44,6 +46,7 @@ def camera_2():
 
 @app.route('/camera_3', methods=['GET'])
 def camera_3():
+    id=8
     """
     This is the heart of our video display. Notice we set the mimetype to 
     multipart/x-mixed-replace. This tells Flask to replace any old images with 
