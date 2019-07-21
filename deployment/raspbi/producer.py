b4b16fb7475871b949138ea1352475f96cc19a50
Collabnix <34368930+collabnix@users.noreply.github.com>
2019-07-21 17:57:10 +0530

    Update producer.py

--- deployment/raspbi/producer.py
+++ deployment/raspbi/producer.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[17]:
@@ -18,7 +17,7 @@ import datetime
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
-import base64 
+import base64
 
 topic = "camera1"
 brokers = ["35.189.130.4:9092"]
@@ -32,7 +31,7 @@ def convert_ts(ts, config):
     #lambda_tz = timezone('US/Pacific')
     tz = timezone(config['timezone'])
     utc = pytz.utc
-    
+
     utc_dt = utc.localize(datetime.datetime.utcfromtimestamp(ts))
 
     localized_dt = utc_dt.astimezone(tz)
@@ -47,49 +46,48 @@ def publish_camera():
     """
 
     # Start up producer
-    
-    
+
+
     producer = KafkaProducer(bootstrap_servers=brokers,
                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-    
+
     camera_data = {'camera_id':"1","position":"frontspace","image_bytes":"123"}
-    
+
     camera = cv2.VideoCapture(0)
-    
+
     framecount = 0
-    
+
     try:
         while(True):
-            
+
             success, frame = camera.read()
-            
+
             utc_dt = pytz.utc.localize(datetime.datetime.now())
             now_ts_utc = (utc_dt - datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds()
-        
+
             ret, buffer = cv2.imencode('.jpg', frame)
-            
+
             camera_data['image_bytes'] = base64.b64encode(buffer).decode('utf-8')
-            
+
             camera_data['frame_count'] = str(framecount)
-            
+
             camera_data['capture_time'] = str(now_ts_utc)
-            
+
             producer.send(topic, camera_data)
-            
+
             framecount = framecount + 1
-            
+
             # Choppier stream, reduced load on processor
             time.sleep(0.2)
-            
-            
-            
+
+
     except Exception as e:
         print((e))
         print("\nExiting.")
         sys.exit(1)
 
-    
+
     camera.release()
     producer.close()
 
@@ -115,7 +113,6 @@ if __name__ == "__main__":
 
 
 # In[ ]:
-
 
 
 
