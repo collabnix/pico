a72730f7e34c6b947147be04a8a87170be56cd1c
Collabnix <34368930+collabnix@users.noreply.github.com>
2019-07-23 14:31:55 +0530

    Update plates-consumer.py

--- deployment/plates/plates-consumer.py
+++ deployment/plates/plates-consumer.py
@@ -14,6 +14,7 @@ import base64
 topic = "testpico"
 brokers = ["35.189.130.4:9092"]
 
+# Initialising Kafka consumer(Lambda) with topic 
 consumer = KafkaConsumer(
     topic, 
     bootstrap_servers=brokers,
@@ -22,28 +23,32 @@ consumer = KafkaConsumer(
 
 # In[18]:
 
-
+# Initialising AWS session using Secrey Keys
 session = boto3.session.Session(aws_access_key_id='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
                                 aws_secret_access_key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
                                 region_name='us-west-2')
 
-
+# Reading every message in the consumer topic(queue) followed by decoding at the producer
 for msg in consumer:
     
     img_bytes = base64.b64decode(msg.value['image_bytes'])
 
+    
+ # Initializing the AWS Rekognition System
     rekog_client = session.client('rekognition')
-    response = rekog_client.detect_text(Image={'Bytes':img_bytes})
+ 
+ # Sending the Image byte array to the AWS Rekognition System to detect the text in the image
+
+    response = rekog_client.detect_text(Image={'Bytes':img_bytes})  
+    
+ # Capturing the text detections from the AWS Rekognition System response
+ 
 
     textDetections=response['TextDetections']
 
     for text in textDetections:
                 print ('Detected text:' + text['DetectedText'])
                 print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-                print ('Id: {}'.format(text['Id']))
-                if 'ParentId' in text:
-                    print ('Parent Id: {}'.format(text['ParentId']))
-                print ('Type:' + text['Type'])
                 
     print("#"*50)
 
