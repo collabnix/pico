9e03ba66dcf07bc5b5c61dadc3dbe26787873f79
Avinash Bendigeri <abendigeri@gmail.com>
2019-08-09 19:55:42 +0530

    Added the object detection python code

---
+++ deployment/objects/consumer.py
@@ -0,0 +1,110 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import datetime
+from flask import Flask, Response, render_template
+from kafka import KafkaConsumer
+import json
+import base64
+
+# Fire up the Kafka Consumer
+camera_topic_1 = "camera1"
+camera_topic_2 = "camera2"
+camera_topic_3 = "camera3"
+brokers = ["35.189.130.4:9092"]
+
+camera1 = KafkaConsumer(
+    camera_topic_1, 
+    bootstrap_servers=brokers,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+camera2 = KafkaConsumer(
+    camera_topic_2, 
+    bootstrap_servers=brokers,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+
+camera3 = KafkaConsumer(
+    camera_topic_3, 
+    bootstrap_servers=brokers,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+
+# Set the consumer in a Flask App
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/camera_1', methods=['GET'])
+def camera_1():
+    id=5
+    """
+    This is the heart of our video display. Notice we set the mimetype to 
+    multipart/x-mixed-replace. This tells Flask to replace any old images with 
+    new values streaming through the pipeline.
+    """
+    return Response(
+        getCamera1(), 
+        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/camera_2', methods=['GET'])
+def camera_2():
+    id=6
+    """
+    This is the heart of our video display. Notice we set the mimetype to 
+    multipart/x-mixed-replace. This tells Flask to replace any old images with 
+    new values streaming through the pipeline.
+    """
+    return Response(
+        getCamera2(), 
+        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/camera_3', methods=['GET'])
+def camera_3():
+    id=8
+    """
+    This is the heart of our video display. Notice we set the mimetype to 
+    multipart/x-mixed-replace. This tells Flask to replace any old images with 
+    new values streaming through the pipeline.
+    """
+    return Response(
+        getCamera3(), 
+        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def getCamera1():
+    """
+    Here is where we recieve streamed images from the Kafka Server and convert 
+    them to a Flask-readable format.
+    """
+    for msg in camera1:
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpg\r\n\r\n' + base64.b64decode(msg.value['image_bytes']) + b'\r\n\r\n')
+
+def getCamera2():
+    """
+    Here is where we recieve streamed images from the Kafka Server and convert 
+    them to a Flask-readable format.
+    """
+    for msg in camera2:
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpg\r\n\r\n' + base64.b64decode(msg.value['image_bytes']) + b'\r\n\r\n')
+            
+            
+def getCamera3():
+    """
+    Here is where we recieve streamed images from the Kafka Server and convert 
+    them to a Flask-readable format.
+    """
+    for msg in camera3:
+        yield (b'--frame\r\n'
+              b'Content-Type: image/jpg\r\n\r\n' + base64.b64decode(msg.value['image_bytes']) + b'\r\n\r\n')           
+            
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
---
+++ deployment/objects/image_processor.py
@@ -0,0 +1,155 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import boto3
+import json
+import cv2
+import decimal
+from copy import deepcopy
+
+from __future__ import print_function
+import base64
+import datetime
+import time
+import decimal
+import uuid
+import json
+import boto3
+import pytz
+from pytz import timezone
+from copy import deepcopy
+
+from PIL import Image, ImageDraw, ExifTags, ImageColor, ImageFont
+
+import datetime
+from kafka import KafkaConsumer
+import boto3
+import json
+import base64 
+
+# Fire up the Kafka Consumer
+topic = "image-pool"
+brokers = ["35.189.130.4:9092"]
+
+consumer = KafkaConsumer(
+    topic, 
+    bootstrap_servers=brokers,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+
+# In[18]:
+
+producer = KafkaProducer(bootstrap_servers=brokers,
+                            value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+
+AWS_ACCESS_KEY_ID = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+AWS_SECRET_ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+
+session = boto3.session.Session(aws_access_key_id = AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+                                region_name='us-west-2')
+
+
+def load_config():
+    '''Load configuration from file.'''
+    with open('image-processor.json', 'r') as conf_file:
+        conf_json = conf_file.read()
+        return json.loads(conf_json)
+
+#Load config
+config = load_config()
+
+def start_processor():
+    
+    for msg in consumer:
+
+        camera_data = {}
+
+        img_bytes = base64.b64decode(msg.value['image_bytes'])
+
+        camera_topic = "camera"+str(msg.value['camera_id'])
+
+        stream = io.BytesIO(img_bytes)
+        image=Image.open(stream)
+
+        imgWidth, imgHeight = image.size  
+        draw = ImageDraw.Draw(image) 
+
+        rekog_client = session.client('rekognition')
+        rekog_max_labels = config["rekog_max_labels"]
+        rekog_min_conf = float(config["rekog_min_conf"])
+
+        label_watch_list = config["label_watch_list"]
+        label_watch_min_conf = float(config["label_watch_min_conf"])
+        label_watch_phone_num = config.get("label_watch_phone_num", "")
+        label_watch_sns_topic_arn = config.get("label_watch_sns_topic_arn", "")
+
+        rekog_response = rekog_client.detect_labels(
+                Image={
+                    'Bytes': img_bytes
+                },
+                MaxLabels=rekog_max_labels,
+                MinConfidence=rekog_min_conf
+            )
+
+        boxes = []
+        objects = []
+        confidence = []
+
+        for label in rekog_response['Labels']:
+
+                for instance in label['Instances']:
+
+                    if(instance['BoundingBox']['Top'] > 0):
+
+                        print("Found a Box of {}".format(label['Name']))
+
+                        top = imgHeight * instance['BoundingBox']['Top']
+                        left = imgWidth * instance['BoundingBox']['Left']
+                        width = imgWidth * instance['BoundingBox']['Width']
+                        height = imgHeight * instance['BoundingBox']['Height']
+
+                        boxes.append([top,left,width,height])
+
+                        objects.append(label['Name'])
+
+                        confidence.append(label['Confidence']))   
+
+
+        for i, box in enumerate(boxes):
+
+        top = box[0]
+        left = box[1]
+        width = box[2]
+        height = box[3]
+
+
+        points = (
+                    (left,top),
+                    (left + width, top),
+                    (left + width, top + height),
+                    (left , top + height),
+                    (left, top)
+
+                )
+
+        font = ImageFont.truetype("arial.ttf", 25)
+        draw.line(points, fill='#00d400', width=3)
+
+        label = str(objects[i])+":"+str(confidence[i])
+        color = 'rgb(255,255,0)' # white color
+        draw.text((left, top - 25), label, fill=color,font=font)
+
+        camera_data['image_bytes'] = image.tobytes()
+
+        producer.send(camera_topic,camera_data)
+        
+
+if __name__ == "__main__":
+    start_processor()
+
