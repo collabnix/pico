c516f8cabd1486af561c68a6a3f5bd4807f1c033
Avinash Bendigeri <abendigeri@gmail.com>
2019-08-12 22:36:23 +0530

    Added the Consumer Poll Funtion Image Processor

--- deployment/objects/image_processor.py
+++ deployment/objects/image_processor.py
@@ -67,96 +67,102 @@ config = load_config()
 
 def start_processor():
     
-    for msg in consumer:
+    while True:
+        
+#         raw_frame_messages = consumer.poll(timeout_ms=10, max_records=10)
+        raw_frame_messages = consumer.poll()
+        
+        for topic_partition, msgs in raw_frame_messages.items():
+            for msg in msgs:
 
-        camera_data = {}
+                camera_data = {}
 
-        img_bytes = base64.b64decode(msg.value['image_bytes'])
+                img_bytes = base64.b64decode(msg.value['image_bytes'])
 
-        camera_topic = "camera"+str(msg.value['camera_id'])
+                camera_topic = "camera"+str(msg.value['camera_id'])
 
-        stream = io.BytesIO(img_bytes)
-        image=Image.open(stream)
+                stream = io.BytesIO(img_bytes)
+                image=Image.open(stream)
 
-        imgWidth, imgHeight = image.size  
-        draw = ImageDraw.Draw(image) 
+                imgWidth, imgHeight = image.size  
+                draw = ImageDraw.Draw(image) 
 
-        rekog_client = session.client('rekognition')
-        rekog_max_labels = config["rekog_max_labels"]
-        rekog_min_conf = float(config["rekog_min_conf"])
+                rekog_client = session.client('rekognition')
+                rekog_max_labels = config["rekog_max_labels"]
+                rekog_min_conf = float(config["rekog_min_conf"])
 
-        label_watch_list = config["label_watch_list"]
-        label_watch_min_conf = float(config["label_watch_min_conf"])
-        label_watch_phone_num = config.get("label_watch_phone_num", "")
-        label_watch_sns_topic_arn = config.get("label_watch_sns_topic_arn", "")
+                label_watch_list = config["label_watch_list"]
+                label_watch_min_conf = float(config["label_watch_min_conf"])
+                label_watch_phone_num = config.get("label_watch_phone_num", "")
+                label_watch_sns_topic_arn = config.get("label_watch_sns_topic_arn", "")
 
-        rekog_response = rekog_client.detect_labels(
-                Image={
-                    'Bytes': img_bytes
-                },
-                MaxLabels=rekog_max_labels,
-                MinConfidence=rekog_min_conf
-            )
-
-        boxes = []
-        objects = []
-        confidence = []
-
-        for label in rekog_response['Labels']:
-
-                for instance in label['Instances']:
-
-                    if(instance['BoundingBox']['Top'] > 0):
-
-                        print("Found a Box of {}".format(label['Name']))
-
-                        top = imgHeight * instance['BoundingBox']['Top']
-                        left = imgWidth * instance['BoundingBox']['Left']
-                        width = imgWidth * instance['BoundingBox']['Width']
-                        height = imgHeight * instance['BoundingBox']['Height']
-
-                        boxes.append([top,left,width,height])
-
-                        objects.append(label['Name'])
-
-                        confidence.append(label['Confidence'])  
-
-
-        for i, box in enumerate(boxes):
-
-            top = box[0]
-            left = box[1]
-            width = box[2]
-            height = box[3]
-
-
-            points = (
-                        (left,top),
-                        (left + width, top),
-                        (left + width, top + height),
-                        (left , top + height),
-                        (left, top)
-
+                rekog_response = rekog_client.detect_labels(
+                        Image={
+                            'Bytes': img_bytes
+                        },
+                        MaxLabels=rekog_max_labels,
+                        MinConfidence=rekog_min_conf
                     )
 
-            font = ImageFont.truetype("arial.ttf", 25)
-            draw.line(points, fill='#00d400', width=3)
+                boxes = []
+                objects = []
+                confidence = []
 
-            label = str(objects[i])+":"+str(confidence[i])
-            color = 'rgb(255,255,0)' # white color
-            draw.text((left, top - 25), label, fill=color,font=font)
+                for label in rekog_response['Labels']:
 
-            
-            imgByteArr = io.BytesIO()
-            image.save(imgByteArr, format=image.format)
-            imgByteArr = imgByteArr.getvalue()
-            
-            
-            camera_data['image_bytes'] = base64.b64encode(imgByteArr).decode('utf-8')
+                        for instance in label['Instances']:
 
-#             print(camera_topic)
-            
-            producer.send(camera_topic,camera_data)
+                            if(instance['BoundingBox']['Top'] > 0):
+
+                                print("Found a Box of {}".format(label['Name']))
+
+                                top = imgHeight * instance['BoundingBox']['Top']
+                                left = imgWidth * instance['BoundingBox']['Left']
+                                width = imgWidth * instance['BoundingBox']['Width']
+                                height = imgHeight * instance['BoundingBox']['Height']
+
+                                boxes.append([top,left,width,height])
+
+                                objects.append(label['Name'])
+
+                                confidence.append(label['Confidence'])  
+
+
+                for i, box in enumerate(boxes):
+
+                    top = box[0]
+                    left = box[1]
+                    width = box[2]
+                    height = box[3]
+
+
+                    points = (
+                                (left,top),
+                                (left + width, top),
+                                (left + width, top + height),
+                                (left , top + height),
+                                (left, top)
+
+                            )
+
+                    font = ImageFont.truetype("arial.ttf", 25)
+                    draw.line(points, fill='#00d400', width=3)
+
+                    label = str(objects[i])+":"+str(confidence[i])
+                    color = 'rgb(255,255,0)' # white color
+                    draw.text((left, top - 25), label, fill=color,font=font)
+
+
+                    imgByteArr = io.BytesIO()
+                    image.save(imgByteArr, format=image.format)
+                    imgByteArr = imgByteArr.getvalue()
+
+
+                    camera_data['image_bytes'] = base64.b64encode(imgByteArr).decode('utf-8')
+
+        #             print(camera_topic)
+
+                    producer.send(camera_topic,camera_data)
         
 
 if __name__ == "__main__":
